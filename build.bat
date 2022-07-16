59f357995664ebc1b652b33d055755163babca47
MiracleLau <zhiminemail@163.com>
2022-07-16 11:27:00 +0800

    修改编译脚本

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 echo publish Windows platform self contained
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/win-x64-nodependencies
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-win-x64-nodependencies
 
 echo publish Windows platform no self contained
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true  --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/win-x64-dependencies
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true  --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-win-x64-dependencies
 
 echo publish linux platform self contained
-dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/linux-x64-nodependencies
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-linux-x64-nodependencies
 
 echo publish linux platform no self contained
-dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/linux-x64-dependencies
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-linux-x64-dependencies
 
 echo publish macos platform self contained
-dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/macos-x64-nodependencies
+dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimmerRemoveSymbols=true --self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-osx-x64-nodependencies
 
 echo publish macos platform no self contained
-dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/macos-x64-dependencies
+dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true --no-self-contained -o RadYanFoFaViewer/bin/Release/net6.0/publish/RadYanFoFaViewer-osx-x64-dependencies
